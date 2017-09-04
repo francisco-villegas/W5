4097b2aefed4d4fedb162b702d3bb7b196f567e4
francisco-villegas <francisco_7766@hotmail.com>
2017-09-04 18:23:37 -0400

    Finishing

--- app/src/main/java/com/example/pancho/w5/view/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/pancho/w5/view/mainactivity/MainActivity.java
@@ -9,19 +9,17 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.pancho.w5.R;
 import com.example.pancho.w5.injection.mainactivity.DaggerMainActivityComponent;
 import com.example.pancho.w5.model.CurrentObservation;
-import com.example.pancho.w5.model.HourlyForecast;
 import com.example.pancho.w5.model.HourlyForecastOrdered;
 import com.example.pancho.w5.util.CONSTANTS;
 import com.example.pancho.w5.view.settingsactivity.SettingsActivity;
@@ -49,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
 
     RecyclerView.LayoutManager layoutManager;
     RecyclerView.ItemAnimator itemAnimator;
+    @BindView(R.id.toolbar_header_view)
+    LinearLayout toolbarHeaderView;
 
     private Toolbar myToolbar;
     private FirstAdapter firstAdapter;
@@ -109,9 +109,28 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
     @Override
     public void sendCurrentWeather(CurrentObservation currentObservation) {
         this.currentObservation = currentObservation;
-        tvTemperatureTop.setText(currentObservation.getTempF() + "°F");
         tvConditionTop.setText(currentObservation.getWeather());
         setTitle(currentObservation.getDisplayLocation().getFull());
+
+        SharedPreferences prefs = getSharedPreferences(CONSTANTS.MY_PREFS, MODE_PRIVATE);
+        String unit = prefs.getString(CONSTANTS.MY_PREFS_UNITS, "Fahrenheit");
+        changeColor(currentObservation, unit);
+    }
+
+    private void changeColor(CurrentObservation currentObservation, String unit) {
+        if (unit.equals("Celsius")) {
+            tvTemperatureTop.setText(currentObservation.getTempC() + "°C");
+        } else {
+            tvTemperatureTop.setText(currentObservation.getTempF() + "°F");
+        }
+
+        if (Double.parseDouble(currentObservation.getTempF().toString()) > 60) {
+            myToolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), CONSTANTS.max_color));
+            toolbarHeaderView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), CONSTANTS.max_color));
+        } else {
+            myToolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), CONSTANTS.min_color));
+            toolbarHeaderView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), CONSTANTS.min_color));
+        }
     }
 
     @Override
@@ -143,16 +162,15 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (resultCode){
+        switch (resultCode) {
             case CONSTANTS.RESULT_BACK:
                 SharedPreferences prefs = getSharedPreferences(CONSTANTS.MY_PREFS, MODE_PRIVATE);
                 String zip = prefs.getString(CONSTANTS.MY_PREFS_ZIP, "");
-                if(zip.equals("")) {
+                if (zip.equals("")) {
 //                    Toast.makeText(this, "Empty zip code", Toast.LENGTH_SHORT).show();
 //                    InvalidOrNullZip();
                     finish();
-                }
-                else {
+                } else {
                     HashMap<String, String> changes = (HashMap<String, String>) data.getSerializableExtra(CONSTANTS.RESULT_BACK_VALUE);
                     String zip_changes = changes.get(CONSTANTS.MY_PREFS_ZIP);
                     String unit_changes = changes.get(CONSTANTS.MY_PREFS_UNITS);
@@ -161,11 +179,7 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
                     else if (unit_changes != null && !unit_changes.equals("")) {
                         firstAdapter.setUnits(unit_changes);
                         firstAdapter.notifyDataSetChanged();
-                        if(unit_changes.equals("Celsius")){
-                            tvTemperatureTop.setText(currentObservation.getTempC() + "°C");
-                        }
-                        else
-                            tvTemperatureTop.setText(currentObservation.getTempC() + "°F");
+                        changeColor(currentObservation, unit_changes);
                     }
                 }
                 break;
